fba0d24cfe4aea5715fbb976a6fe7ed4d099da54
Jezuz <Jezuz@DESKTOP-RN4DP0B>
2024-02-20 01:40:05 -0600

    Agregar archivos de proyecto.

---
+++ problema fisica2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace problema_fisica2
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            masa resul = new masa();
+            resul.resultado(42,12);
+            Console.WriteLine(" =" + resul.dencidad * resul.metros3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ problema fisica2/masa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema_fisica2
+{
+    internal class masa
+    {
+        public masa() 
+        {
+            Console.WriteLine("la masa del objeto es");
+        }
+        public double metros3;
+        public double dencidad;
+
+        public double Metros3 { get => metros3; set => metros3 = value; }
+        public double Dencidad { get => dencidad; set => dencidad = value; }
+
+        public void resultado(double metros3,double dencidad)
+        {
+            this.metros3 = metros3;
+            this.dencidad = dencidad;
+        }
+    }
+}
